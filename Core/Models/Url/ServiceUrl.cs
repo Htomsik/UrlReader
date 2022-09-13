4f7994b3b9a106df42fea6c232f8dc188856c5b1
Konstantin <83224669+Htomsik@users.noreply.github.com>
2022-09-13 05:24:41 +0300

    Add Urls models

---
+++ Core/Models/Url/ServiceUrl.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace Core.Models;
+
+public sealed class ServiceUrl : Url
+{
+
+    /// <summary>
+    ///     count of <a> tags
+    /// </summary>
+    [Reactive]
+    public int TagsCount { get; set; } = default;
+
+    /// <summary>
+    ///     State of Url
+    /// </summary>
+    [Reactive]
+    public UrlState State { get; set; } = UrlState.Unknown;
+}
---
+++ Core/Models/Url/Url.cs
@@ -0,0 +1,13 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Core.Models;
+
+public class Url : ReactiveObject
+{
+    /// <summary>
+    ///     Path to site
+    /// </summary>
+    [Reactive]
+    public string Path { get; set; }
+}
